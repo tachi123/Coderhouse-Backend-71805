14e0133b3a6dbcfba5eee13010754ab5108b7e06
Jaime <jnramirezlorca@gmail.com>
2025-01-08 07:38:36 -0300

    Initial commit

---
+++ Clase-00/index.js
@@ -0,0 +1,6 @@
+console.log('abc');
+console.warn("texto con warn");
+console.log(1);
+console.log(null);
+console.log(undefined);
+console.log([1,2,3,4]); //array
---
+++ Clase-00/variables.js
@@ -0,0 +1,26 @@
+//Definición de variables
+let nombre = 'Juan';
+let edad = 25;
+let precio = 50.75;
+let series = ['Breaking Bad', 'Stranger Things'];
+
+//Mostrar valores por consola
+console.log('Nombre: ', nombre);
+console.log('Edad: ', edad);
+console.log('Precio: ', precio);
+console.log('Series: ', series);
+
+//Incrementar la edad en 1
+edad++; //edad = edad + 1;
+
+//Añadir una serie
+series.push('Game of Thrones');
+
+//Reasignar valor a la variable edad
+edad = "S/D";
+
+//Mostrar las variables actualizadas
+console.log('-- Después de las actualizaciones--');
+console.log('Edad: ', edad);
+console.log('Series: ', series);
+

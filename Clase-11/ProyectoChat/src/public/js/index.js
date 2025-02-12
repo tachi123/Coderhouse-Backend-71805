8b4c377d074b1ce3eff2a71afef87688e0d7269a
Juli <jnramirezlorca@gmail.com>
2025-02-12 08:50:49 -0300

    agrego alerta básica

--- Clase-11/ProyectoChat/src/public/js/index.js
+++ Clase-11/ProyectoChat/src/public/js/index.js
@@ -1,8 +1,18 @@
 /** En este archivo, nos posicionamos como "clientes" */
-const io = io();
+const socket = io();
 
 /**
- * io hace referencia a "socket.io", se llama así por convención
- * Nos permite instanciar el socket y guardarlo en la constante "io"
+ * socket hace referencia a "socket.io", se llama así por convención
+ * Nos permite instanciar el socket y guardarlo en la constante "socket"
  * Este socket es el que vamos a utilizar para poder comunicarnos con el socket del servidor
  */
+
+Swal.fire({
+    title: "Hola, Coders",
+    text: "Alerta básica con Sweetalert2",
+    icon: "success"
+})
+
+socket.on('messages', data => {
+
+});
